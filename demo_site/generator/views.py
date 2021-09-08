20902cba11be707f467c50d417cb250fbf3c39f0
BradLin0819 <brad19970819@gmail.com>
2021-09-08 23:12:35 +0800

    add option buttons

--- demo_site/generator/views.py
+++ demo_site/generator/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'generator/index.html')
+    return render(request, 'index.html')
 
 
 def generate_citation_text(request):
-    print(request.POST.get('citing_context'))
-    return redirect('/')
+    print(request.POST.get('citing_input_type'))
+    return render(request, 'feedback.html')
